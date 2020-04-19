fa7cdfe621a507f364afa327bbcd62a8fa2e75ac
Fernando Meira <thomwork@outlook.com>
2020-04-19 11:36:42 -0300

    feat: 🎸 Added .stylelintrc.json

--- src/components/Perfil/styles.js
+++ src/components/Perfil/styles.js
@@ -3,14 +3,13 @@ import styled from 'styled-components';
 import { pxToRem } from '../../functions';
 
 export const Container = styled.div`
+  width: ${pxToRem(200)};
+  height: ${pxToRem(200)};
   margin: ${pxToRem(50)} auto;
 
   display: flex;
-  align-items: center;
   justify-content: center;
-
-  width: ${pxToRem(200)};
-  height: ${pxToRem(200)};
+  align-items: center;
 
   img {
     border-radius: 50%;
